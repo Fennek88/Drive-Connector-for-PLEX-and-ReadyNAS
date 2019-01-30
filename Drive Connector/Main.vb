94d7b3a8b2938de9e638ce34bb8bbfa9d5f6c505
Andre von Tongelen <andrevt@t-online.de>
2019-01-30 10:26:58 +0100

    Removed Sleep from Main

--- Drive Connector/Main.vb
+++ Drive Connector/Main.vb
@@ -1,5 +1,4 @@
 ﻿Public Class Main
-    Private Declare Sub Sleep Lib "kernel32" (ByVal dwMilliseconds As Long)
     Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
         If ComboBox1.Text = "" Then
             MsgBox("No drive letter selected!")
@@ -11,6 +10,6 @@
     Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
         Call getfreedrives()
     End Sub
-    
+
 End Class
 
